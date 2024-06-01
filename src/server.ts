4821a0797a22c05fbccc2c25d2fc2646ea21cfa4
Gloria Hornero <snootchie.boochies@gmail.com>
2024-06-01 20:10:36 +0200

    refactors server file

--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,17 @@ app.use('/bookings', bookingsRouter)
 
 app.use(defaultErrorHandler);
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
-app.listen(PORT, () => {
-    console.log('We are listening....');
-});
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error(`Error starting server: ${error}`);
+        process.exit(1);
+    }
+}
+
+startServer();
